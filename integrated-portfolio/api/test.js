791ce086c76f8370b7f4934f77c7bffd95b82e10
creationsstudio <johnccreations21@gmail.com>
2026-02-21 15:06:14 -0500

    refactor: Clean up repository for optimal Vercel deployment

--- integrated-portfolio/api/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200);
-  res.end(JSON.stringify({ 
-    message: 'Test endpoint working',
-    method: req.method,
-    timestamp: new Date().toISOString()
-  }));
-};
