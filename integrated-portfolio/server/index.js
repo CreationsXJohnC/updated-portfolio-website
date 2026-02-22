588d15e3eac72bd3381b738069f7561b5f4f242d
creationsstudio <johnccreations21@gmail.com>
2026-02-21 19:23:29 -0500

    fix: adopt vercel-native monorepo build configuration

--- integrated-portfolio/server/index.js
+++ integrated-portfolio/server/index.js
@@ -381,12 +381,7 @@ async function startServer() {
     }
   });
 
-  // Root endpoint
-  app.use(express.static(path.join(__dirname, '../client')));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/index.html'));
-    });
 
   // Start HTTP server
   await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
