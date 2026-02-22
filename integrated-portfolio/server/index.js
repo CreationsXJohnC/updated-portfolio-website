2186198cd65faeb0f70e96588dd56fc58d4548b5
creationsstudio <johnccreations21@gmail.com>
2026-02-21 19:17:18 -0500

    fix: align with vercel zero-config by using public output directory

--- integrated-portfolio/server/index.js
+++ integrated-portfolio/server/index.js
@@ -382,10 +382,10 @@ async function startServer() {
   });
 
   // Root endpoint
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../client')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/index.html'));
     });
 
   // Start HTTP server
